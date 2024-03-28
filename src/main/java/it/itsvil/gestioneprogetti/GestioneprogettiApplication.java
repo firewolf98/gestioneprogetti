ac7df361b6e15ec30e29acd015aa508d903a8216
Luca Esposito <luca.esposito.98@outlook.it>
2024-03-28 14:34:21 +0100

    first commit

---
+++ src/main/java/it/itsvil/gestioneprogetti/GestioneprogettiApplication.java
@@ -0,0 +1,13 @@
+package it.itsvil.gestioneprogetti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneprogettiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestioneprogettiApplication.class, args);
+    }
+
+}
